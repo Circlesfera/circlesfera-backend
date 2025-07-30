2e95235cf02b44e8e0fc0383eb00982939b8fa91
ShadyFeliu <shadyfeliu@gmail.com>
2025-07-30 20:32:37 +0200

    feat(post): modelo simple y modular de Post con referencia a usuario, imagen, caption y likes

---
+++ src/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    maxlength: 2200
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
